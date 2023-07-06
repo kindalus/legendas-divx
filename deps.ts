13c7ad05cc82b105e0cf537023b394fefe9bbf63
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2023-07-06 18:47:47 +0100

    qui 06 jul 2023 18:47:47 WAT

---
+++ deps.ts
@@ -0,0 +1,17 @@
+export * as path from "https://deno.land/std@0.193.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.193.0/fs/mod.ts";
+export { parse } from "https://deno.land/std@0.193.0/flags/mod.ts";
+
+export { default as unrar } from "npm:node-unrar-js@0.8.1";
+export { default as yauzl } from "npm:yauzl@2.10.0";
+
+// {
+// 	"imports": {
+// 		"path": ,
+// 		"fs": "https://deno.land/std@0.193.0/fs/mod.ts",
+// 		"flags": "https://deno.land/std@0.193.0/flags/mod.ts",
+
+// 		"node-unrar-js": "npm:node-unrar-js@0.8.1",
+// 		"yauzl": "npm:yauzl@2.10.0"
+// 	}
+// }
